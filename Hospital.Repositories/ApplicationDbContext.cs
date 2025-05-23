0de9f51f07033564472eacc11bb6168fd3daa977
rana-621 <ranaebrahim621@gmail.com>
2025-05-23 23:51:26 +0300

    Install Packages

--- Hospital.Repositories/ApplicationDbContext.cs
+++ Hospital.Repositories/ApplicationDbContext.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Hospital.Repositories
 {
-    internal class ApplicationDbContext
+    class ApplicationDbContext : IdentityDbContext
     {
     }
 }
