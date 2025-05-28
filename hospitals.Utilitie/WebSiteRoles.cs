eed19f777e0e36f684f0a85d350197c5992d66ce
rana-621 <ranaebrahim621@gmail.com>
2025-05-28 23:50:28 +0300

    edit websiteroles

--- hospitals.Utilitie/WebSiteRoles.cs
+++ hospitals.Utilitie/WebSiteRoles.cs
@@ -2,6 +2,9 @@
 {
     public class WebSiteRoles
     {
-
+        public const string Website_Admin = "Admin";
+        public const string Website_Doctor = "Doctor";
+        public const string Website_Patient = "Patient";
     }
+
 }
