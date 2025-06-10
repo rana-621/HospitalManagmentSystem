c1a67c348b206ad9738334ece3946e6d903dfc70
rana-621 <ranaebrahim621@gmail.com>
2025-06-10 23:56:18 +0300

    update..

--- hospitals.Utilitie/DbInitializer.cs
+++ hospitals.Utilitie/DbInitializer.cs
@@ -1,7 +1,12 @@
-﻿namespace hospitals.Utilities;
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace hospitals.Utilities;
 
 public class DbInitializer : IDbInitializer
 {
+    private UserManager<IdentityUser> _userManager;
+    private RoleManager<IdentityRole> _roleManager;
+    private readonly ApplicationDbContext _context;
     public void Intailize()
     {
         throw new NotImplementedException();
