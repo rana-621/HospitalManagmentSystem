ceaeaa8783e497dbd63e3ae2ec3b7bd9bb3a5a01
rana-621 <ranaebrahim621@gmail.com>
2025-06-11 11:54:38 +0300

    Intialize database classes and ApplicationUser , install packages AspNetCore

--- Hospital.Repositories/ApplicationDbContext.cs
+++ Hospital.Repositories/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Hospital.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hospital.Repositories
@@ -9,5 +10,8 @@ namespace Hospital.Repositories
             : base(options)
         {
         }
+
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
--- hospitals.Utilitie/DbInitializer.cs
+++ hospitals.Utilitie/DbInitializer.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Hospital.Models;
+using Hospital.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace hospitals.Utilities;
 
@@ -7,8 +10,50 @@ public class DbInitializer : IDbInitializer
     private UserManager<IdentityUser> _userManager;
     private RoleManager<IdentityRole> _roleManager;
     private readonly ApplicationDbContext _context;
+
+    public DbInitializer(UserManager<IdentityUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        ApplicationDbContext context)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _context = context;
+    }
+
+
     public void Intailize()
     {
-        throw new NotImplementedException();
+        try
+        {
+            if (_context.Database.GetPendingMigrations().Count() > 0)
+            {
+                _context.Database.Migrate();
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+
+        if (!_roleManager.RoleExistsAsync(WebSiteRoles.Website_Admin).GetAwaiter().GetResult())
+        {
+            _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.Website_Admin)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.Website_Patient)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(WebSiteRoles.Website_Doctor)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = "raEbrahim",
+                Email = "ranaebrahim621@gmail.com"
+
+            }, "ranona&621farag").GetAwaiter().GetResult();
+
+            var Appuser = _context.ApplicationUsers.FirstOrDefault(u => u.Email == "ranaebrahim621@gmail.com");
+
+            if (Appuser != null)
+            {
+                _userManager.AddToRoleAsync(Appuser, WebSiteRoles.Website_Admin).GetAwaiter().GetResult();
+            }
+        }
     }
 }
