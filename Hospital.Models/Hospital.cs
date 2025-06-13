5133cfa2638ef1e487328f025c331a5289cb1afc
rana-621 <ranaebrahim621@gmail.com>
2025-06-13 23:08:33 +0300

    Hospital Model Properities

---
+++ Hospital.Models/Hospital.cs
@@ -0,0 +1,15 @@
+﻿namespace Hospital.Models;
+
+public class Hospital
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+
+    public string PinCode { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public ICollection<Room> Rooms { get; set; }
+    public ICollection<Contacts> Contacts { get; set; }
+
+}
