cc5ff59f48aefd8a4034e5c9de2645fcaeac050d
rana-621 <ranaebrahim621@gmail.com>
2025-06-21 15:12:41 +0300

    Prepate Room ViewModel

---
+++ Hospital.ViewModels/RoomViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Hospital.Models;
+
+namespace Hospital.ViewModels;
+
+public class RoomViewModel
+{
+    public int Id { get; set; }
+    public string RoomNumber { get; set; }
+    public string Type { get; set; }
+    public string Status { get; set; }
+    public string HospitalInfoId { get; set; }
+
+    public RoomViewModel()
+    {
+    }
+    public RoomViewModel(Room model)
+    {
+        Id = model.Id;
+        RoomNumber = model.RoomNumber;
+        Type = model.Type;
+        Status = model.Status;
+        HospitalInfoId = model.HospitalId;
+    }
+
+    public Room ConvertViewModel(RoomViewModel model)
+    {
+        return new Room
+        {
+            Id = model.Id,
+            RoomNumber = model.RoomNumber,
+            Type = model.Type,
+            Status = model.Status,
+            HospitalId = model.HospitalInfoId
+        };
+    }
+}
