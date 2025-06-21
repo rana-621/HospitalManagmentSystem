cba5d32bf9c35c90a2b15ea26c9e006604ef87f5
rana-621 <ranaebrahim621@gmail.com>
2025-06-21 06:07:29 +0300

    HospitalViewModel

--- Hospital.ViewModels/HospitalInfoViewModel.cs
+++ Hospital.ViewModels/HospitalInfoViewModel.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Hospital.ViewModels
+using Hospital.Models;
+
+namespace Hospital.ViewModels;
+
+public class HospitalInfoViewModel
 {
-    internal class HospitalInfoViewModel
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+
+    public string PinCode { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+
+    public HospitalInfoViewModel()
     {
+
+    }
+    public HospitalInfoViewModel(HospitalInfo model)
+    {
+        Id = model.Id;
+        Name = model.Name;
+        Type = model.Type;
+        City = model.City;
+        PinCode = model.PinCode;
+        Country = model.Country;
+
+    }
+    public HospitalInfo ConvertViewModel(HospitalInfoViewModel model)
+    {
+        return new HospitalInfo
+        {
+            Id = model.Id,
+            Name = model.Name,
+            Type = model.Type,
+            City = model.City,
+            PinCode = model.PinCode,
+            Country = model.Country,
+        };
+
     }
 }
