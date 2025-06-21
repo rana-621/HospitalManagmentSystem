d5581f6f048a4407cfc111c83175c4b8fd229b85
rana-621 <ranaebrahim621@gmail.com>
2025-06-21 16:30:13 +0300

    Prepare Room Controller with CRUD operations

---
+++ Hospital.Web/Areas/Admin/Controllers/RoomsController.cs
@@ -0,0 +1,59 @@
+﻿using Hospital.Services;
+using Hospital.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Web.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class RoomsController : Controller
+    {
+        private IRoomService _room;
+
+        public RoomsController(IRoomService room)
+        {
+            _room = room;
+        }
+        public IActionResult Index(int pageNumber = 1, int pageSize = 10)
+        {
+            return View(_room.GetAll(pageNumber, pageSize));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(RoomViewModel roomViewModel)
+        {
+
+            _room.InsertRoom(roomViewModel);
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var room = _room.GetRoomById(id);
+
+            return View(room);
+        }
+        [HttpPost]
+        public IActionResult Edit(RoomViewModel roomViewModel)
+        {
+
+            _room.UpdateRoom(roomViewModel);
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _room.DeleteRoom(id);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
