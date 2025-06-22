701025593c208f546ffe581455920d02c7cbc1d0
rana-621 <ranaebrahim621@gmail.com>
2025-06-22 23:47:57 +0300

    Create new controller for the ContactsController

---
+++ Hospital.Web/Areas/Admin/Controllers/ContactsController.cs
@@ -0,0 +1,62 @@
+﻿using Hospital.Services;
+using Hospital.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Hospital.Web.Areas.Admin.Controllers;
+
+[Area("admin")]
+public class ContactsController : Controller
+{
+    private IContactService _contact;
+    private IHospitalInfo _hospitalInfo;
+
+    public ContactsController(IContactService contact, IHospitalInfo hospitalInfo)
+    {
+        _contact = contact;
+        _hospitalInfo = hospitalInfo;
+    }
+    public IActionResult Index(int pageNumber = 1, int pageSize = 10)
+    {
+        return View(_contact.GetAll(pageNumber, pageSize));
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(ContactViewModel contactViewModel)
+    {
+
+        _contact.InsertContact(contactViewModel);
+        return RedirectToAction("Index");
+
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        ViewBag.hopital = new SelectList(_hospitalInfo.GetAll(), "Id", "Name");
+        var viewModel = _contact.GetContactById(id);
+
+        return View(viewModel);
+    }
+    [HttpPost]
+    public IActionResult Edit(ContactViewModel contactViewModel)
+    {
+
+        _contact.UpdateContact(contactViewModel);
+        return RedirectToAction("Index");
+
+    }
+
+    public IActionResult Delete(int id)
+    {
+        _contact.DeleteContact(id);
+        return RedirectToAction("Index");
+
+    }
+}
--- Hospital.Web/Areas/Admin/Controllers/RoomsController.cs
+++ Hospital.Web/Areas/Admin/Controllers/RoomsController.cs
@@ -2,58 +2,57 @@
 using Hospital.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Hospital.Web.Areas.Admin.Controllers
+namespace Hospital.Web.Areas.Admin.Controllers;
+
+[Area("admin")]
+public class RoomsController : Controller
 {
-    [Area("admin")]
-    public class RoomsController : Controller
+    private IRoomService _room;
+
+    public RoomsController(IRoomService room)
     {
-        private IRoomService _room;
+        _room = room;
+    }
+    public IActionResult Index(int pageNumber = 1, int pageSize = 10)
+    {
+        return View(_room.GetAll(pageNumber, pageSize));
+    }
 
-        public RoomsController(IRoomService room)
-        {
-            _room = room;
-        }
-        public IActionResult Index(int pageNumber = 1, int pageSize = 10)
-        {
-            return View(_room.GetAll(pageNumber, pageSize));
-        }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+    [HttpPost]
+    public IActionResult Create(RoomViewModel roomViewModel)
+    {
 
-        [HttpPost]
-        public IActionResult Create(RoomViewModel roomViewModel)
-        {
+        _room.InsertRoom(roomViewModel);
+        return RedirectToAction("Index");
 
-            _room.InsertRoom(roomViewModel);
-            return RedirectToAction("Index");
+    }
 
-        }
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var room = _room.GetRoomById(id);
 
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            var room = _room.GetRoomById(id);
+        return View(room);
+    }
+    [HttpPost]
+    public IActionResult Edit(RoomViewModel roomViewModel)
+    {
 
-            return View(room);
-        }
-        [HttpPost]
-        public IActionResult Edit(RoomViewModel roomViewModel)
-        {
+        _room.UpdateRoom(roomViewModel);
+        return RedirectToAction("Index");
 
-            _room.UpdateRoom(roomViewModel);
-            return RedirectToAction("Index");
+    }
 
-        }
+    public IActionResult Delete(int id)
+    {
+        _room.DeleteRoom(id);
+        return RedirectToAction("Index");
 
-        public IActionResult Delete(int id)
-        {
-            _room.DeleteRoom(id);
-            return RedirectToAction("Index");
-
-        }
     }
 }
