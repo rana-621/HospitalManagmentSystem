c5c728c5e7c6c09e68f1dc507ddf5bb17d7d4c49
rana-621 <ranaebrahim621@gmail.com>
2025-06-25 23:06:38 +0300

    Timing Model

---
+++ Hospital.Models/Timing.cs
@@ -0,0 +1,26 @@
+﻿namespace Hospital.Models
+{
+
+    public class Timing
+    {
+        public int Id { get; set; }
+        public ApplicationUser DoctorId { get; set; }
+        public DateTime Date { get; set; }
+        public int MorningShiftStartTime { get; set; }
+        public int MorningShiftEndTime { get; set; }
+        public int AfternoonShiftStartTime { get; set; }
+        public int AfternoonShiftEndTime { get; set; }
+        public int Duration { get; set; } // Duration in minutes
+        public Status Status { get; set; }
+    }
+}
+namespace Hospital.Models
+{
+    public enum Status
+    {
+        Available,
+        Pending,
+        Confirmave,
+
+    }
+}
